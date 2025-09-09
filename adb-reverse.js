a2a8e8447a9d77a5d50fde0be674e290ad3f7bcd
csjhonathan <jhonathancarv.s@gmail.com>
2025-09-09 13:13:53 -0300

    ajusta script para rodar o projeto no android

--- adb-reverse.js
+++ adb-reverse.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const { execSync } = require('child_process');
 
-const deviceId = process.env.DEVICE_ID;
+let deviceId = process.env.DEVICE_ID;
 
 if (!deviceId) {
-	console.error('❌ DEVICE_ID não definido no .env');
-	process.exit(1);
+	deviceId = 'emulator-5554';
+	console.error('❌ DEVICE_ID não definido no .env, usando dispositivo padrão: emulator-5554');
 }
 
 try {
@@ -13,5 +13,8 @@ try {
 	console.log(`✅ Porta 8081 redirecionada com sucesso para o dispositivo ${ deviceId }`);
 } catch (err) {
 	console.error('❌ Erro ao executar o comando adb:', err.message);
+	deviceId = 'emulator-5554';
+	console.error('❌ Erro ao executar o comando adb, usando dispositivo padrão: emulator-5554');
+	execSync(`adb -s ${ deviceId } reverse tcp:8081 tcp:8081`, { stdio: 'inherit' });
 	process.exit(1);
 }
